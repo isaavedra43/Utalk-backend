4dedd70f4f299c353e8eaa855ea4122e745c47f1
isaavedra43 <53502011+isaavedra43@users.noreply.github.com>
2025-07-04 11:17:03 -0600

    1

--- constants.js
+++ constants.js
@@ -5,7 +5,17 @@ module.exports = {
   TWILIO_WHATSAPP_NUMBER: 'whatsapp:+1234567890',
 
   FIREBASE_SERVICE_ACCOUNT: {
-    // Copia aquí tu JSON de Service Account
-  },
+      "type": "service_account",
+      "project_id": "utalk-a893a",
+      "private_key_id": "ec9688c3bc5b2679bab2eb64e6c836e80909c417",
+      "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC4ePbLVEF1h3Iq\ngRu0nPxQ27USB1Q4q8WelFtZ0j8mlWYKoakhGofUSZTMxLSnUX0+XiKfM/fq8+EK\n+4dwlzC1vLtyZS2SFvsUru8KnMxpSv+us10NCCKPvHO7HDe5H9Gf0j0YLDF8rBsX\nDW4UObCl6oBSDpDW5jGIDsy4K8zd/SZGUI+XATodaVI+xWX/ElSCakt3790YSX3C\njYKuGFv0glGlhr6IusYNhGKz8fS4ZCLxwNNHL1uUcjBgtGUkNyrXIpNEiDsJmCS5\nUNaznOkP61Q+t9X16L1zJhgJaQH7DqJ4XFaCrs1NLfzWf0N1DtsJXdna4IPyw0oo\nlksc9aUvAgMBAAECggEAEqw9QGAIejxEzFRM+M9/44gXKuUusMfZFVAWHnWflSkV\nRFPl2fx6Av0/FdUpe4WjpFmvWSclBsA7UjsbaW8HFrZX0QtjKAEHLFT4Ib5fxfQP\nWos8YDJ9G8iV0bsC/ExiEd1G+DiXpR40ux94ZOK2ugIOjX/SSHZ8+AqWIrmy9Jtc\nRJkzHsJ2UOPASElgAPeTa9x/MUTOQbKfWMFVom5JwQg5e0mOph2pHwcQxS+RTMwW\nEWzjwtr39oEppgKzgi+oDA2huvjdD7zaKF4nXGRCE9VaK0rJ0otG/88XJhey/s7I\nh9kQKdWFbl/FruHg1VUxf10Q0Nxdo/C07y+Tug9wRQKBgQDyfKg5TQwN8gJxihhb\n5L7L+BnUKDQkcagWhb4xm2ay3BOskK8wbPVsT8rW6NJuR1FjkvjkF0dsoB1Kdpbs\nXc0BtuOaETwn8iJgOjDlX9GMwsrsY8hNMCHlRSi9YVvIW/jg8Sqt+HvxxdGxIG3R\nqCj6pDDzucx2NFUI2PBB6EtWwwKBgQDCwKrWRJjRMv6+BlOQSwEscpJc6XuJ4tRG\nr2O7W+2Ty1LcBtoDtHqmfU3p0JpOcJutO4LlXOmUnBWNlOUQs7fGiiFU7m35gksi\n0kzQaIM8p2Xa1cxkwvtGF/odO7qTHIsaCSe9gtVtXVdKKWEZVLo83Hle/wCzYuUA\nZn5S4BHJJQKBgQDNjMIFILyxLY7ItCkF7JeXy04Ju7LHmdjEjsa0wmiCAXBj/Zmj\n0RkRoWnuVxnaBXaGk9WLc/hFqkxqDLDO2IypgWDZ/vYhXm2paGp+ZYT6bz0YuVTX\nqY9yC/gG4PquktgbxhNJubNHNUsVfMnayVNDc99TGxKouJ/EijDPjZwfhQKBgEUn\n9YIwybM/0IXZ6UKL39bQuHiV0HCeQ5L2JQwbELoysTHAnRpDtGn6hPhKQwEnq6BQ\nSaL441kKM/0aqlzL9K5jzoJCSHsZcWTkGLLI6hFXwCt9UB9XvbKeQQqwcwhaUmrk\nZWA+XVuAkcab+wbrt1Z1VAQPuUf6DieePa1gM/ChAoGBAKhBtv2MSNNyqIrAoGg8\nEUG4lQDHf09NAJ69H5Om1cp8tBUMuEIUIGs9MT1CkLu6fcJ4ar7BIGO1KmgIvTw5\n2NVZLDoOZjEAVbVvx2ayyAWFNFFKjHLF9hxqrBykH/InmOiZsYBe+arZFzv77B5d\nEOT8u2/Wyr3+rW9hg47wnEJi\n-----END PRIVATE KEY-----\n",
+      "client_email": "firebase-adminsdk-fbsvc@utalk-a893a.iam.gserviceaccount.com",
+      "client_id": "107693281402764477091",
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40utalk-a893a.iam.gserviceaccount.com",
+      "universe_domain": "googleapis.com"
+      },
   FIREBASE_DATABASE_URL: 'https://<TU_PROJECT_ID>.firebaseio.com'
 };
