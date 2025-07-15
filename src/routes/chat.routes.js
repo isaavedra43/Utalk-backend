d0b892c42568b95fdef405acd82e5a9c5a9117b3
isaavedra43 <53502011+isaavedra43@users.noreply.github.com>
2025-07-15 16:28:00 -0600

    reinicio

--- src/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const chatController = require('../controllers/chat.controller');
-
-router.post('/webhook', chatController.receiveIncoming);
-router.post('/send', chatController.sendOutgoing);
-router.get('/messages', chatController.listLast);
-
-module.exports = router;
